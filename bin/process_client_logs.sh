21d317f56b1da0eda73f9f302da6e896b57a2bd6
Joshua Eklund <eklun124@rynite.morris.umn.edu>
2021-09-09 10:44:46 -0500

    Add code to process_client_logs.sh
    This commit adds the code necessary for process_client_logs to function partially. There is a problem with the regex expression. It does not catch lines that only have one number for the date ex: Aug 1 and it also does not catch lines with numbers in the username ex: test10.
    
    Co-authored-by: Dante Miller <mill8622@morris.umn.edu>

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -2,6 +2,6 @@
 
 directory=$1
 
-cd "$directory"
+cd "$directory"/var/log
 
-cat * |  awk 'match($0, /([a-zA-z]{3} [0-9]{2}) ([0-9]{2})[0-9:]+ ([a-zA-z]+).+ Failed password .+ ([a-zA-z ]+) from ([0-9.]+)/, groups) {print groups[1] groups[2] groups[3] groups[4] groups[5] "\n"}' > failed_login_data.txt
+cat * | awk 'match($0, /([a-zA-z]{3} [0-9]{2}) ([0-9]{2})[0-9:]+ [a-zA-z]+.+ Failed password .+ ([a-zA-z ]+) from ([0-9.]+)/, groups) {print groups[1] " " groups[2] " " groups[3] " " groups[4]}' >  failed_login_data.txt | mv failed_login_data.txt  ../../ 
