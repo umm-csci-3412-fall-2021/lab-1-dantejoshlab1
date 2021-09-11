d4042ebb1cad9b2c9670ecebc0025e1388b19b42
Joshua Eklund <eklun124@rynite.morris.umn.edu>
2021-09-10 19:17:14 -0500

    Fix comments in process_client_logs.sh
    This commit fixes the commments in process_client_logs.sh so that they are more readable.
    
    Co-authored-by: Dante Miller <mill8622@morris.umn.edu>

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -7,7 +7,10 @@ directory=$1
 cd "$directory"/var/log || exit
 
 
-#Concatenate all files in the directory and then pipe that into an awk command that uses the following regex expression to match the required lines and then prints the capture groups in that regex expression and put that into a file called failed_login_data.txt
+#Concatenate all files in the directory
+#pipe cat into an awk command that uses the following regex expression to match the required lines
+#print the capture groups in that regex expression
+#put that into a file called failed_login_data.txt
 cat ./* | awk 'match($0, /([a-zA-z]{3} [ 0-9]{2}) ([0-9]{2})[0-9:]+ [a-zA-Z]+.+ Failed password .+ ([a-zA-Z0-9 ]+) from ([0-9.]+)/, groups) {print groups[1] " " groups[2] " " groups[3] " " groups[4]}' >  failed_login_data.txt
 
 
