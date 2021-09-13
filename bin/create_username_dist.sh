fe922b9ad6af11d1da1b7a4a46a2d50942a52dd9
Joshua Eklund <eklun124@rynite.morris.umn.edu>
2021-09-12 20:14:51 -0500

    Finish implementing create_username_dist.sh
    This commit finishes the implementation of the create_username_dist.sh script. This commit adds the code necessary so that the script can call wrap contents and generate the proper html output. We also added the code so that the script makes a tmp file to work in and then delete after.
    
    Co-authored-by: Dante Miller <mill8622@morris.umn.edu>o

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -2,7 +2,12 @@
 
 directory=$1
 
+tempFile=$(mktemp /tmp/tmpFile.XXXXXX)
 cd "$directory" || exit
-find .  -name "failed_login_data.txt" -exec cat {} +  | awk 'match($0, /[a-zA-z]{3} [0-9]{2} [0-9]{2} [+ [a-zA-z]+ ([a-zA-z0-9 ]+) /, groups) {print groups[1]}' > output2.txt
-sort output2.txt | uniq -c | awk 'match($0, / + ([0-9]+) ([a-zA-Z]+)/, groups) {print "data.addRow([\x27"groups[2]"\x27, " groups[1]"]);"}' > output4.txt  
+find .  -name "failed_login_data.txt" -exec cat {} +  | awk 'match($0, /[a-zA-Z]{3} [0-9 ]+ ([a-zA-Z0-9\w-]+)/, groups) {print groups[1]}' | sort| uniq -c | awk 'match($0, / + ([0-9]+) ([a-zA-Z0-9\w-]+)/, groups) {print "data.addRow([\x27"groups[2]"\x27, " groups[1]"]);"}' > "$tempFile"
+
+cd - || exit
+./bin/wrap_contents.sh "$tempFile"  html_components/username_dist data/username_dist.html
+rm "$tempFile"
+
 
