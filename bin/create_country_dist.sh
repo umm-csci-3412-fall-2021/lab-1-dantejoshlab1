5f4725cd6600f606eae45c6f433ffd2c9738f3a0
Joshua Eklund <eklun124@rynite.morris.umn.edu>
2021-09-16 10:44:51 -0500

    Fix comments in create_country_dist.html
    This commmit fixes the existing comments in create_country_dist.html and adds some new ones for clarification.
    
    Co-authored-by: Dante Miller <mill8622@morris.umn.edu>

--- bin/create_country_dist.sh
+++ bin/create_country_dist.sh
@@ -3,7 +3,7 @@
 #Assign value of first argument to variable directory
 directory=$1
 
-#Make a temporary file
+#Make temporary files
 tempFile=$(mktemp /tmp/tmpFile.XXXXXX)
 tempFile2=$(mktemp /tmp/tmpFile2.XXXXXX)
 
@@ -12,19 +12,27 @@ cd "$directory" || exit
 
 #Find all files that have name "failed_login_data.txt" in the directory
 #After all files with that name have been, concatenate them into one file
-#Pipe into awk command that gets the all the usernames from the combined file
-#Sort that file
-#Count up how many times each user name appears
-#Pipe into awk command that grabs the username and the number of times it appears
-#Print that and put it in the tempFile
+#Pipe into awk command that gets the all the IP addresses from the combined file
+#Sort IP addresses 
+#Put that output in the tempFile
 find .  -name "failed_login_data.txt" -exec cat {} +  | awk 'match($0, /[a-zA-Z]{3} [0-9 ]+ [a-zA-Z0-9\w-]+ ([0-9.]+)/, groups) {print groups[1]}' | sort -t . -k 1 > "$tempFile"
 
-cd - || exit
-join "$tempFile" ./etc/country_IP_map.txt | awk '{print $2}' |  sort | uniq -c | awk '{print "data.addRow([\x27"$2"\x27, " $1"]);"}'  > "$tempFile2"
 
-#Call wrap_contents to wrap the tempFile in between the username_dist header and footer
-#Put that in a file called username_dist.html in the data directory
+#cd into previous working directory or exit if failure
+cd - || exit
+
+#Join the temp file with the country_IP_map file to map IP addresses to country codes
+#Pipe into awk command that grabs the second column, the country codes, and prints those
+#Sort the country codes 
+#Count how many times the country codes show up
+#Pipe into awk command that grabs the country codes and number of occurences and prints them in the proper format
+#Put that in that in tempFile2
+join "$tempFile" ./etc/country_IP_map.txt |  awk '{print $2}' |  sort | uniq -c | awk '{print "data.addRow([\x27"$2"\x27, " $1"]);"}'  > "$tempFile2"
+
+#Call wrap_contents to wrap tempFile2 in between the country_dist  header and footer
+#Put that in a file called country_dist.html in the data directory
 ./bin/wrap_contents.sh "$tempFile2"  html_components/country_dist data/country_dist.html
 
+#Remove the temporary files
 rm "$tempFile"
 rm "$tempFile2"
